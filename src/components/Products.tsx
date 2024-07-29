49def9e1669d7fe5d43d299419b513e8d5c18802
Mark <mjmags2020@gmail.com>
2024-07-30 00:01:27 +0800

    pagination and infinite scroll using useInfiniteQuery

---
+++ src/components/Products.tsx
@@ -0,0 +1,44 @@
+import { Fragment } from "react/jsx-runtime";
+import { useProduct, useProducts } from "../services/queries";
+import { useState } from "react";
+
+export default function Products() {
+  const [selectedProductId, setSelectedProductId] = useState<number | null>(
+    null
+  );
+  const productsQuery = useProducts();
+  const productQuery = useProduct(selectedProductId);
+  console.log("productsQuery.data?.pages", productsQuery.data?.pages);
+  return (
+    <>
+      {productsQuery.data?.pages.map((group, index) => (
+        <Fragment key={index}>
+          {group.map((product) => (
+            <Fragment key={product.id}>
+              <button onClick={() => setSelectedProductId(product.id)}>
+                {product.name}
+              </button>
+              <br />
+            </Fragment>
+          ))}
+        </Fragment>
+      ))}
+      <br />
+      <div>
+        <button
+          onClick={() => productsQuery.fetchNextPage()}
+          disabled={
+            !productsQuery.hasNextPage || productsQuery.isFetchingNextPage
+          }
+        >
+          {productsQuery.isFetchingNextPage
+            ? "Loading more..."
+            : productsQuery.hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </button>
+      </div>
+      <div>Selected Product: {productQuery.data?.name}</div>
+    </>
+  );
+}
